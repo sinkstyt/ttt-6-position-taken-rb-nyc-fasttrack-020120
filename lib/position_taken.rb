e265a47c6db5f9312226f3d88485cea4e5c8eb07
Tyler Sinks <tyler.sinks@gmail.com>
2020-02-02 19:28:37 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?
-    array[index_number] != " "
+    if array[index_number] == X
+        return TRUE
+    if array[index_number] == O
+        return true
+
 end
